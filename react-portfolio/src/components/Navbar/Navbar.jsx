c448c46f117f76f3c1af4c9d2ef8c937493763cd
akila199910 <akeeumayanga7775@gmail.com>
2024-01-28 22:34:54 +0530

    adding menuicon button and modify mobile friendly navbar componenent

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import style from "./Navbar.module.css";
+import { getImageUrl } from '../../utils';
 
 function Navbar() {
+    const[menuOpen,setMenuOpen]=useState(false);
   return (
     <nav className={style.navbar}>
         <a className={style.title} href="/">Portfolio</a>
         <div className={style.menu}>
-            <ul className={style.menuItems}>
+            <img className={style.menuBtn}
+            src={menuOpen ? getImageUrl("nav/closeIcon.png")
+                          : getImageUrl("nav/menuIcon.png")}
+            alt="menu-button" 
+            onClick={()=>setMenuOpen(!menuOpen)}
+            />
+            <ul className={`${style.menuItems} ${menuOpen && style.menuOpen}`}
+            onClick={()=>setMenuOpen(false)}>
                 <li><a href="#about">About</a></li>
             
                 <li><a href="#experience">Experience</a></li>
