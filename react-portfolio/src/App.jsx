a5c2bea0026efd041e7ca6fbc1bf6838b0a00f2a
akila199910 <akeeumayanga7775@gmail.com>
2024-01-29 00:46:06 +0530

    create hero component

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,13 +1,14 @@
 
 import style from './App.module.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 function App() {
 
   return (
     <div className={style.App}>
          <Navbar/>
-
+        <Hero/>
     </div>
   )
 }
