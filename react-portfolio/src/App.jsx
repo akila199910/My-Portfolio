671413584a420116a857747024961ddcb34692b1
akila199910 <akeeumayanga7775@gmail.com>
2024-02-01 22:47:24 +0530

    Update App.jsx

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Experience from './components/Experience/Experience'
+import Project from './components/Project/Project'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Hero/>
         <About/>
         <Experience/>
+        <Project/>
     </div>
   )
 }
